e9d253b8e8d6841e461d749033dd168072686087
Glendich <glendacand@gmail.com>
2016-04-11 22:56:00 -0400

    assignment 2 added functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The followinf functions are used to cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inv <<- solve
+    getsolve <- function() inv
+    list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
-
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If that's the case, it gets the result skipping the
+# computation. Otherwise, it computes the inverse and sets the value in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getsolve()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    ## Return a matrix that is the inverse of 'x'
+    data <- x$get()
+    inv <- solve(data)
+    x$setsolve(inv)
+    inv
 }
+
+
+        
+
